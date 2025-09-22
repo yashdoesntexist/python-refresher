eaf9bc312189c4f1c5d764b87b3b746fc5d4bc42
agentP <learnhacksandtricks@gmail.com>
2025-09-21 23:12:55 -0700

    lab2

---
+++ lab2.py
@@ -0,0 +1,20 @@
+from mlxtend.frequent_patterns import apriori, association_rules
+import pandas as pd
+
+# Create a small dataset:
+df = pd.DataFrame([
+  [1,1,1,0],   # Basket 1
+  [0,1,0,1],   # Basket 2
+  [1,1,1,1],   # Basket 3
+], columns=['milk','bread','eggs','butter'])
+
+frequentItemset = apriori(df, use_colnames= True) # min_support defaults to 0.5 no need to explicitly mention that
+association = association_rules(frequentItemset, metric='lift', min_threshold=1.0)
+
+print(frequentItemset)
+print()
+print(association)
+
+# //Use apriori() with min_support=0.5 to find frequent itemsets.
+# // Generate association rules with association_rules() using metric="lift" and min_threshold=1.0.
+# //Print the frequent itemsets and the rules (show antecedents, consequents, support, confidence, lift).
