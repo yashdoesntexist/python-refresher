5b664e845518989e062c2c80aae74fc3620b2d48
agentP <learnhacksandtricks@gmail.com>
2025-09-21 23:41:42 -0700

    nlp

---
+++ nplLab1.py
@@ -0,0 +1,12 @@
+import re
+import playwright
+import pycorenlp
+from collections import Counter
+import pandas as pd
+import pdfplumber
+
+print(dir(playwright))
+
+print(dir(pycorenlp))
+
+print(dir(pdfplumber))
