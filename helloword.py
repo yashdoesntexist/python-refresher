4810d01e5ee85da9197db6f0e250b04c9ae6bce8
agentP <learnhacksandtricks@gmail.com>
2025-09-21 22:08:06 -0700

    initial commit

---
+++ helloword.py
@@ -0,0 +1,35 @@
+
+# newVar = "hello world"
+# print(newVar)
+
+# firstName = "Tonty"
+# lastName = "Stark"
+# isGenius = True
+
+# # name = input("what is your name")
+
+# # print(name)
+
+# age = input("Your age")
+
+
+# nextYearAge = int(age) + 1
+
+# print(nextYearAge)
+
+# first = input("enter num")
+# second = input("enter num 2")
+
+# sum = int(first) + int(second)
+
+# print(sum)
+
+# strings
+
+name = "Bruce Wayne"
+
+print(name.upper())
+
+print(name.find('W'))
+
+print(name.replace("Bruce", ""))
